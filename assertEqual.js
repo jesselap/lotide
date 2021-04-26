a794d7320974a4c3bb6340c8da488c5c4863603b
Jesse <jesse.a.lapointe@gmail.com>
2021-04-26 18:54:50 +0000

    custom assertion function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return '🔴🔴🔴 Assertion Failed: ' + actual + ' !== ' + expected;
+  } else {
+    return '🟢🟢🟢 Assertion Passed: ' + actual + ' === ' + expected;
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
