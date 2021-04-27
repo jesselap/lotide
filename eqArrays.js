3477587c0d89da54ff53782422b4956fab9d1faa
Jesse <jesse.a.lapointe@gmail.com>
2021-04-27 21:13:50 +0000

    removed assertEqual code

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    return `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`;
-  } else {
-    return `🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`;
-  }
-};
 
 const eqArrays = function(array1, array2) {
   for (let index of array1) {
@@ -17,10 +10,3 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true))
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)) 
-
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true))
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false))
