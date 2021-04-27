67c7be4ee4aa4d8c3ba0a81cc203506c21ed9270
Jesse <jesse.a.lapointe@gmail.com>
2021-04-27 22:07:35 +0000

    removing unwanted values from an array

---
+++ without.js
@@ -0,0 +1,14 @@
+const without = function(source, itemsToRemove) {
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        source.splice(i, 1);
+      }
+    }
+  }
+  console.log(source);
+};
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+without(['hello', 'my', 'dear', 'friend'], ['2', 3, 'dear']);
