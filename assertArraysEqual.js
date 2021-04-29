27b96d2132f1cea4778c3c1999169f66570f47f4
Jesse <jesse.a.lapointe@gmail.com>
2021-04-29 19:43:37 +0000

    second commit took out second for loop

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,9 @@
-
 const assertArraysEqual = function(array1, array2) {
   let match = true;
   for (let i of array1) {
-    for (let j of array2) {
-      if (array1[i] !== array2[j]) {
-        match = false;
-      }
+    if (array1[i] !== array2[i]) {
+      match = false;
+      
     }
   }
   if (!match) {
