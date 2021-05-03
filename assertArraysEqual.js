06fdb3355a9b3f1ee9ca704c8cebe537b5fb51b0
Jesse <jesse.a.lapointe@gmail.com>
2021-05-03 18:12:10 +0000

    first commit making test modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,12 @@
+const eqArrays = require('./eqArrays.js');
+
 const assertArraysEqual = function(array1, array2) {
-  let match = true;
-  for (let i of array1) {
-    if (array1[i] !== array2[i]) {
-      match = false;
-      
-    }
-  }
+  let match = eqArrays(array1, array2);
   if (!match) {
     console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
   } else {
     console.log( `🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
   }
 };
+
 module.exports = assertArraysEqual;
