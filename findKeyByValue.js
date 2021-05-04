da23b97428f95e792739f1725e68fafe2d016dab
Jesse <jesse.a.lapointe@gmail.com>
2021-05-04 00:20:20 +0000

    refactored so it will output answer if there is more than one corresponding key

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,16 @@
 const findKeyByValue = function(object, value) {
   let keys = Object.keys(object);
   let values = Object.values(object);
+  let answer = [];
   for (let i = 0; i < values.length; i++) {
     if (values[i] === value) {
-      return keys[i];
+      answer.push(keys[i]);
     }
   }
+  if (answer.length < 2) {
+    answer = answer.join('');
+  }
+  return answer;
 };
 
 module.exports = findKeyByValue;
